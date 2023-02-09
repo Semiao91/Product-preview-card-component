60f1c01df0e9bf2b2e239ef5efec0ae022e02a2b
Bernardo <berna.1991.1@gmail.com>
2023-02-08 20:18:29 -0800

    third commit

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Logo from "../assets/icon-cart.svg"
 
 export default function Button() {
 
     return (
         <div className="Button">
-                <img alt="shoppingcart" src="src\assets\icon-cart.svg" />
+                <img alt="shoppingcart" src={Logo} />
                 <div className="addtoCart">Add to Cart</div>        
         </div>
     )
